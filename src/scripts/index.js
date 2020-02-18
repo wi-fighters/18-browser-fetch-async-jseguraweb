72102d317b775d3f150a9e3942baca7af094e257
Jaime Segura <jseguraweb@gmail.com>
2020-02-18 11:47:17 +0100

    solve exercise

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,41 @@
 import "../styles/main.scss";
 import "babel-polyfill"
 
-const modal = document.getElementById("myModal");
-let promiseOfModal = new Promise(function (resolve) {
-    window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
-});
+const modal = document.getElementById('myModal');
+const closeModal = document.querySelector('.close');
+const button = document.getElementById("continue");
 
-promiseOfModal.then(function(val) {
-    console.log("User has been on the page for 60 seconds");
-    val.style.display = "block";
-})
+const modalFunction = async () => {
+    // create the promise. whatever you pass inside the resolve method is going to get filtered throw the next steps
+    let promiseOfModal = new Promise(function (resolve) {
+        window.setTimeout(function () {
+            resolve(modal);
+        }, 60*1000);
+    });
 
-modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
-        case "close":
-        case "modal":
-        modal.style.display = "none"
-        break;
-    }
-})
+    let modalMessage = await promiseOfModal; // promiseOfModal is modal, which we passed as an argument in the resolve method. And we store it in a new variable (modalMessage)
+    modalMessage.style.display = "block";
+};
 
+// we call the function
+modalFunction();
+
+// event listener to close the modal
+closeModal.addEventListener('click', () => modal.style.display = 'none');
+
+
+async function popUpFunction() {
+    // create a promise
+    let buttonPromise = new Promise(function (resolve, reject) {
+        setTimeout(() => {  
+            resolve("Please subscribe"); // we pass this string
+        }, 200)
+    });
+
+    button.style.backgroundColor = "blue";
+    button.style.color = "white";
+    let popUp = await buttonPromise;
+    alert(popUp);
+}
+
+button.addEventListener("click", popUpFunction); // we call the function with a callback inside the event listener. When we click, it will run
